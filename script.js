f199d19a15b4bb549eebb35b150f0618a5c0e78d
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-01-18 20:14:32 +0530

    Added the general scripts for the project...

---
+++ script.js
@@ -0,0 +1,6 @@
+const search = document.getElementById("search"),
+    submit = document.getElementById("submit"),
+    random_Btn = document.getElementById("random"),
+    result_heading = document.getElementById("result-heading"),
+    mealsEl = document.getElementById("meals"),
+    singleMealEl = document.getElementById("meals");
