65926e9d778bf44832af96b75770c0e9bc5441a7
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-01-18 20:31:08 +0530

    Added the scripts for the fetching the search term from the mealdb API...

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 const search = document.getElementById("search"),
-    submit = document.getElementById("submit"),
-    random_Btn = document.getElementById("random"),
-    result_heading = document.getElementById("result-heading"),
-    mealsEl = document.getElementById("meals"),
-    single_mealEl = document.getElementById("single-meal");
+  submit = document.getElementById("submit"),
+  random_Btn = document.getElementById("random"),
+  result_heading = document.getElementById("result-heading"),
+  mealsEl = document.getElementById("meals"),
+  single_mealEl = document.getElementById("single-meal");
+
+function searchMeal(e) {
+  e.preventDefault();
+
+  single_mealEl.innerHTML = "";
+
+  const term = search.value;
+  if (term.trim()) {
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  } else {
+    alert("Please enter something to seach for you meal....");
+  }
+}
+
+submit.addEventListener("submit", searchMeal);
